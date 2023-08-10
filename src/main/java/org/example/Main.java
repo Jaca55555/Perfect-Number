54b29aa67a30731df11011bf0f33217ab21420cb
jaca55555 <jahongitToshpulatov@mail.ru>
2023-08-10 15:29:48 +0500

    ArrangingCoins

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,39 +6,19 @@ import java.util.Scanner;
 
 public class Main {
 
-    public int reverse(int x) {
-            int reversed = 0;
-
-            while (x != 0) {
-                int lastDigit = x % 10;
-
-                // Check for overflow/underflow before updating reversed
-                if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && lastDigit > 7)) {
-                    return 0; // Overflow will occur
-                }
-                if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && lastDigit < -8)) {
-                    return 0; // Underflow will occur
-                }
-
-                reversed = reversed * 10 + lastDigit;
-                x = x / 10;
-            }
-
-            return reversed;
+    public int arrangeCoins(int n) {
+        int i = 0;
+        while (n >= i) {
+            n -= i;
+            i++;
         }
-
-
+        return i-1;
     }
 
-
-
     public static void main(String[] args) {
-        int a=123;
-        Main main=new Main();
-        System.out.println(main.reverse(a));
-
-
-
+        int a = 8;
+        Main main = new Main();
+        System.out.println(main.arrangeCoins(a));
 
 
     }
